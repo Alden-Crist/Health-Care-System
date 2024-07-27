888a7faa870cd161b826f9c5ae483efa12ad08a7
Alden-Crist <aldencristrego08@gmail.com>
2024-07-27 14:27:36 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -6,21 +6,21 @@ dotenv.config({ path: "././config.env" });
 
 const app = require("./app");
 
-//LOCAL DATABASE CONNECTION
-mongoose.connect(process.env.DATABASE_LOCAL).then(() =>
-  console.log("DB connection successfull !!(localhost)")
+// //LOCAL DATABASE CONNECTION
+// mongoose.connect(process.env.DATABASE_LOCAL).then(() =>
+//   console.log("DB connection successfull !!(localhost)")
+// );
+
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
 );
 
-
-// const DB = process.env.DATABASE.replace(
-//   '<PASSWORD>',
-//   process.env.DATABASE_PASSWORD
-// );
-
-// //Atlas DATABASE CONNECTION
-// mongoose.connect(DB).then(() =>
-//   console.log('DB connection successfull(Atlas)')
-// );
+//Atlas DATABASE CONNECTION
+mongoose.connect(DB).then(() =>
+  console.log('DB connection successfull(Atlas)')
+);
 
 
 const port = process.env.PORT  || 8000;
